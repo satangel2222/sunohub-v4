e3f8a2df2deb37b0a6e42889ec7f72ca738d9295
frankdelamo <774frank5@gmail.com>
2026-01-02 10:39:26 +0800

    debug: probe env vars

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,8 @@
 export default function handler(request, response) {
-    return response.status(200).json({ message: "Reverted Hello Check" });
+    return response.status(200).json({
+        message: "Env Probe",
+        hasUrl: !!process.env.VITE_SUPABASE_URL,
+        hasKey: !!process.env.VITE_SUPABASE_ANON_KEY,
+        envKeys: Object.keys(process.env).filter(k => k.startsWith('VITE_'))
+    });
 }
