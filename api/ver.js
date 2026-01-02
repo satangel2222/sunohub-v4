aa2bc37f1ba635e7ebe742e8f446026bbb6ccf13
frankdelamo <774frank5@gmail.com>
2026-01-02 10:51:13 +0800

    chore: remove debug functions

--- api/ver.js
+++
@@ -1,9 +0,0 @@
-export default function handler(request, response) {
-    return response.status(200).json({
-        version: process.version,
-        hasFetch: typeof fetch !== 'undefined',
-        env: {
-            hasUrl: !!process.env.VITE_SUPABASE_URL
-        }
-    });
-}
