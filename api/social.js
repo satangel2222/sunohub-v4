41667b1c3990bfd1ff52a15a639f4eca0a937a6e
frankdelamo <774frank5@gmail.com>
2026-01-02 10:32:24 +0800

    refactor: rename social to share and enable full logic

--- api/social.js
+++
@@ -1,8 +0,0 @@
-export default function handler(request, response) {
-    return response.status(200).json({
-        body: request.body,
-        query: request.query,
-        cookies: request.cookies,
-        message: "I am social cloned from hello"
-    });
-}
